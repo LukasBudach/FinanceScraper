27ec4c10acf16e135d3d709da9aaf4e2b3d61e24
LukasBudach <lukas.budach@student.hpi.de>
2019-01-29 15:17:30 +0100

    adapt run_tests script to return 1 on test failure

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,10 @@
 import unittest
+import sys
 
 loader = unittest.TestLoader()
 start_dir = 'tests/'
 suite = loader.discover(start_dir)
 
-unittest.TextTestRunner().run(suite)
+results = unittest.TextTestRunner().run(suite)
+
+sys.exit(0 if results.failures.__len__() == 0 else 1)
